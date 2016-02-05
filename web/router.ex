ba6847221b93bc8d8c56ef819c4d33f736ac3d28
Riza Fahmi <rizafahmi@gmail.com>
2016-02-05 16:29:54 +0700

    Initial commit

---
+++ web/router.ex
@@ -0,0 +1,26 @@
+defmodule GuardianOfGalaxy.Router do
+  use GuardianOfGalaxy.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", GuardianOfGalaxy do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", GuardianOfGalaxy do
+  #   pipe_through :api
+  # end
+end
